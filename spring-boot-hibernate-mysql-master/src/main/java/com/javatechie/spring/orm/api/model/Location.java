d605660869758375da728188132a2321a4f480cf
Subrata Kumar Debata <debata.subrata@gmail.com>
2020-02-29 16:48:27 +0530

    Update Location.java
    Add @GeneratedValue(strategy = GenerationType.IDENTITY)
    to resolve generate key automatically.

--- spring-boot-hibernate-mysql-master/src/main/java/com/javatechie/spring/orm/api/model/Location.java
+++ spring-boot-hibernate-mysql-master/src/main/java/com/javatechie/spring/orm/api/model/Location.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 
 public class Location {
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String state;
 	private String region;
