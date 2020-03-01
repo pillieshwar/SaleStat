840607f1a72545a9a5c60b4150ed40b9c6c8b967
pillieshwar <pillieshwar@gmail.com>
2020-03-01 14:26:01 +0530

    added generationType in doctor, location and person model

--- spring-boot-hibernate-mysql-master/src/main/java/com/javatechie/spring/orm/api/model/DoctorDetails.java
+++ spring-boot-hibernate-mysql-master/src/main/java/com/javatechie/spring/orm/api/model/DoctorDetails.java
@@ -2,6 +2,7 @@ package com.javatechie.spring.orm.api.model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 
--- spring-boot-hibernate-mysql-master/src/main/java/com/javatechie/spring/orm/api/model/Location.java
+++ spring-boot-hibernate-mysql-master/src/main/java/com/javatechie/spring/orm/api/model/Location.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
