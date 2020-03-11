9b6820da7199a710be46b1fa3ef0a79f473f50f0
pillieshwar <pillieshwar@gmail.com>
2020-03-12 01:20:07 +0530

    TICKET-02200306 : UI Created for new doctor page(multiple medicine input pending)

--- spring-boot-hibernate-mysql-master/src/main/java/com/javatechie/spring/orm/api/controller/AdminController.java
+++ spring-boot-hibernate-mysql-master/src/main/java/com/javatechie/spring/orm/api/controller/AdminController.java
@@ -6,8 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AdminController {
 
-	@RequestMapping("/add_medicine") //redirects to the login page
+	@RequestMapping("/add_medicine") //redirects to add new medicine page
     public String addMedicine(){
         return ("add_medicine");
     }
+	
+	@RequestMapping("/add_doctor") //redirects to add new doctor page
+    public String addDoctor(){
+        return ("add_doctor");
+    }
+	
 }
